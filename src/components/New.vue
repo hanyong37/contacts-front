a440c59b12c40da78bb3e83160e325a0c88d604d
hanyong37 <chenxi0809@gmail.com>
2017-08-21 17:13:14 +0800

    create contact ok;

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,47 +1,58 @@
 <template>
   <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
-  <h2>New Contacts</h2>
+  <h2>Create New Contacts</h2>
 
-  <form>
+  <form @submit='create' >
     <div class="form-group">
       <label for="exampleInputEmail1">First Name</label>
-      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter First Name">
+      <p>{{ contact.first_name }}</p>
+      <input type="text" class="form-control" id="" v-model='contact.first_name' :value='contact.first_name' placeholder="Enter First Name">
     </div>
 
     <div class="form-group">
       <label for="exampleInputEmail1">Last Name</label>
-      <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Last Name">
+      <input type="text" class="form-control" id="" v-model='contact.last_name' :value='contact.last_name' placeholder="Enter Last Name">
     </div>
 
 
     <div class="form-group">
       <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+      <input type="text" class="form-control" id="" v-model='contact.email' :value='contact.email' placeholder="Enter Email">
     </div>
 
     <div class="form-group">
       <label for="exampleTextarea">Description</label>
-      <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+      <textarea class="form-control" v-model='contact.description' :value='contact.description' id="exampleTextarea" rows="3"></textarea>
     </div>
 
-    <div class="form-check">
-      <label class="form-check-label">
-        <input type="checkbox" class="form-check-input">
-        Check me out
-      </label>
-    </div>
-
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <router-link class='btn btn-secondary' to='/contacts'>Back to List</router-link>
+    <button type='submit' class="btn btn-primary">Create</button>
   </form>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'new',
+  name: 'edit',
+  methods: {
+    create: function () {
+      console.log('button clicked' + this.name)
+      this.$resource(this.contactUrl()).save(this.contact).then(
+        response => {
+          alert('created contact success!')
+        },
+        () => { console.log('failed') }
+      )
+    }
+  },
   data () {
     return {
-      msg: 'This is new page!!'
+      contact: {
+        first_name: '',
+        last_name: '',
+        email: '',
+        description: ''
+      }
     }
   }
 }
